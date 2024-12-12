9ae9ffe2d15dc07c1cab111b36ee3e07eb05d5d2
user1190150 <besirevic205@gmail.com>
2024-12-12 17:57:00 +0100

    [Feature] Implementierung von PostValidate-SystemEvent und Benutzerdefiniertem Validierer für den Login-Prozess.
    - Hinzugefügt: Attribut `benutzername` im BenutzerController zur
    Zwischenspeicherung des Benutzernamens.
    - Implementiert: Methode `postValidateName` zum Abfangen des
    PostValidate-Events und Zwischenspeichern des Benutzernamens.
    - Registriert: `postValidateName` als Listener-Methode für die
    Benutzername-Komponente in der View.
    - Erstellt: Benutzerdefinierter Validierer für Passwortüberprüfung, der
    den zwischengespeicherten Benutzernamen verwendet.
    - Refakturiert: Login-Prozess zur Unterstützung des neuen
    Validierungsansatzes mit einer feldübergreifenden Validierungsmethode.
    - Test(nur BlackBox): Validierung erfolgreich getestet und fehlerfrei
    integriert.

--- jsf-harisb/src/main/java/de/harisb/jsf/beans/BenutzerController.java
+++ jsf-harisb/src/main/java/de/harisb/jsf/beans/BenutzerController.java
@@ -4,17 +4,28 @@ import java.io.Serializable;
 import java.util.List;
 
 import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.component.UIComponent;
+import jakarta.faces.component.UIInput;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.event.AbortProcessingException;
+import jakarta.faces.event.ComponentSystemEvent;
+import jakarta.faces.validator.ValidatorException;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 
 @Named
 @SessionScoped
 public class BenutzerController implements Serializable {
+	private static final long serialVersionUID = 1L;
 	
 	@Inject
 	private BenutzerVerwaltung benutzerVerwaltung;
 	private Benutzer aktuellerBenutzer;
-	private static final long serialVersionUID = 1L;
+	//Dieses Attribut ist zum Zwischenspeichern des Benutzernamen.
+	//1. Attribut zum Zwischenspeichern des Benutzernamens im Controller hinzufügen.
+	private String benutzername;
+	
 	
 	public BenutzerController() {
 		System.out.println("Benutzer: Konstruktor wurde aufgerufen!");
@@ -29,6 +40,15 @@ public class BenutzerController implements Serializable {
 		this.aktuellerBenutzer = aktuellerBenutzer;
 	}
 	
+	public String getBenutzername() {
+		return benutzername;
+	}
+	
+	public void setBenutzername(String benutzername) {
+		this.benutzername = benutzername;
+	}
+	
+	 
 	public String login() {
 		List<Benutzer> benutzerListeDurchlaufen = benutzerVerwaltung.getBenutzerList();
 		System.out.println("Login Methode: Hier in der Liste sind: " + benutzerListeDurchlaufen.size());
@@ -41,5 +61,27 @@ public class BenutzerController implements Serializable {
 		System.out.println("Nicht Gefunden!");
 		return "login.xhtml";
 	}
+	
+	//2. Listener-Methode im Controller zum Abfangen des PostValidate-Events implementiert (ZWISCHENSPEICHERN)
+	public void postValidateName(ComponentSystemEvent event) throws AbortProcessingException {
+		
+		UIInput temp = (UIInput) event.getComponent();		
+		
+		this.benutzername = (String) temp.getValue();
+		System.out.println("SystemEvent abgefangen. Benutzername: " + this.benutzername + " Zwischengespeichert.");
+	}
+	
+	//4. Im Passwort-Validator des Controllers anhand des gespeicherten Attributs(benutzername) validieren.
+	public void validatePassword(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+		
+		List<Benutzer> bliste = benutzerVerwaltung.getBenutzerList();
+		for (Benutzer b : bliste) {
+			Benutzer temp = new Benutzer(this.benutzername, (String) value);
+			if(b.equals(temp)) 
+				return;
+		} throw new ValidatorException(new FacesMessage("Login falsch!"));
+		
+	}
+	
 
 }
